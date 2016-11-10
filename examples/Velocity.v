ca951bb08a924fe60d21fda7b33811da3b4af1dc
Valentin Robert <valentin.robert.42@gmail.com>
2016-11-10 12:04:22 -0800

    mostly cosmetic changes

--- examples/Velocity.v
+++ examples/Velocity.v
@@ -36,7 +36,7 @@ Section VelocityBound.
      also sets the timer to zero. *)
   Definition ctrl : ActionProp :=
     "a" ::= ***;;
-     ? get "v" [+] get "a"[*]pure d [<=] pure V;;
+    ? get "v" [+] get "a" [*] pure d [<=] pure V;;
     "t" ::= pure 0.
 
   (* The physical dynamics of the system. This formulation
@@ -49,7 +49,8 @@ Section VelocityBound.
   Definition plant : ActionProp :=
     d["v"] [=] #[get "a"] //\\
     d["a"] [=] pure 0 //\\
-    d["t"] [=] pure 1 & get "t" [<=] pure d.
+    d["t"] [=] pure 1
+    & get "t" [<=] pure d.
 
   (* Theorem expressing that the system does enforce
      the upper bound on velocity. The proof is fairly simple
@@ -60,37 +61,64 @@ Section VelocityBound.
   Theorem bound_correct :
     |-- safe -->> [[(ctrl;; plant)^*]]safe.
   Proof.
-    intros. charge_intros. rewrite <- star_I.
+    charge_intros.
+    (* induction rule for the Kleene star *)
+    rewrite <- star_I.
     charge_split.
-    { charge_clear. apply G_rule. charge_intro.
-      unfold ctrl. repeat rewrite seq_rule.
-      rewrite nondet_assign_rule. charge_intros.
-      rewrite test_rule. rewrite assign_rule.
-      rewrite Subst_limpl. charge_intro.
+    { charge_clear.
+      apply G_rule.
+      charge_intro.
+      (* Cut-point: we need to prove that safe is preserved by one step *)
+      unfold ctrl.
+      repeat rewrite seq_rule.
+      (* 1: non-deterministic assignment *)
+      rewrite nondet_assign_rule.
+      charge_intros.
+      (* 2: test *)
+      rewrite test_rule.
+      rewrite assign_rule.
+      rewrite Subst_limpl.
+      charge_intro.
       assert (x <= 0 \/ 0 <= x) by psatzl R.
+      set (EQ1 := get "v" [+] get "a" [*] pure d [<=] pure V).
       destruct H; unfold plant.
-      { rewrite <- differential_cut with (C:=get "a" [<=] pure 0).
-        repeat rewrite Subst_land. charge_split.
+      set (DV := d[ "v"] [=] #[ get "a"]).
+      set (DA := d[ "a"] [=] pure 0).
+      set (DT := d[ "t"] [=] pure 1).
+      {
+        (* ⟦dF & Q⟧ C ∧ ⟦dF & Q ∧ C⟧ P ⊢ ⟦dF & Q⟧ P *)
+        rewrite <- differential_cut with (C := get "a" [<=] pure 0).
+        repeat rewrite Subst_land.
+        charge_split.
         { diff_ind. }
-        { unfold safe. diff_ind. } }
+        { unfold safe.
+          diff_ind. } }
       { rewrite <- differential_cut with (C:=pure 0 [<=] get "a").
-        repeat rewrite box_land. repeat rewrite Subst_land. charge_split.
+        repeat rewrite box_land.
+        repeat rewrite Subst_land.
+        charge_split.
         { diff_ind. }
         { rewrite <- differential_cut
-          with (C:=get "v" [+] get "a"[*](pure d [-] get "t")
-                       [<=] pure V).
-          repeat rewrite Subst_land. charge_split.
+          with (C := get "v" [+] get "a"[*] (pure d [-] get "t") [<=] pure V).
+          repeat rewrite Subst_land.
+          charge_split.
           { diff_ind.
             (* This goal requires non-linear arithmetic,
                so we use a foundational non-linear decision
                procedure. In the future, we will call better
                decision procedures such as Z3. *)
-            breakAbstraction. intros. psatz R. }
-          { rewrite <- differential_weakening'. charge_clear.
+            breakAbstraction.
+            intros.
+            psatz R. }
+          { rewrite <- differential_weakening'.
+            charge_clear.
             do 2 rewrite <- Subst_ltrue.
             apply Proper_Subst_lentails; [ reflexivity | reflexivity | ].
             apply Proper_Subst_lentails; [ reflexivity | reflexivity | ].
-            apply G_rule. breakAbstraction. intros. psatz R. } } } }
+            apply G_rule.
+            breakAbstraction.
+            intros.
+            psatz R. } } } }
     { reflexivity. }
   Qed.
 
